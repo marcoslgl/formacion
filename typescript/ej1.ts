5eee06dea29be00382f66ff35d27a430e9ef47b3
marcoslgl <marcoslgl2004@gmail.com>
2026-03-05 13:08:50 +0100

    feat(typescript): add exercise 1

---
+++ typescript/ej1.ts
@@ -0,0 +1,33 @@
+interface Animal {
+  name: string;
+  canEat: boolean;
+  canDrink: boolean;
+  canSleep: boolean;
+  canFly: boolean;
+}
+
+interface Dog extends Animal {
+  race: string;
+  age: number;
+}
+
+const bird: Animal = {
+  name: "Pájaro",
+  canEat: true,
+  canDrink: true,
+  canSleep: true,
+  canFly: true,
+};
+
+const dog: Dog = {
+  name: "Perro",
+  canEat: true,
+  canDrink: true,
+  canSleep: true,
+  canFly: false,
+  race: "Labrador",
+  age: 5,
+};
+
+console.log(bird);
+console.log(dog);
