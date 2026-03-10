84c74de22691ac1dd922e1e2a573d24db840f3b8
marcoslgl <marcoslgl2004@gmail.com>
2026-03-10 09:28:33 +0100

    feat(vue-nuxt) add exercise 1

---
+++ nuxt-vue-exercises/layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// Aquí irá la lógica global del layout si la necesitas
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <header class="p-4 bg-white shadow-sm">
+      <nav class="container mx-auto">
+        <h1 class="text-xl font-bold text-slate-800">Nuxt 4 Exercises</h1>
+      </nav>
+    </header>
+
+    <main class="flex-grow container mx-auto p-4">
+      <slot />
+    </main>
+
+    <footer class="p-4 text-center text-sm text-gray-500">&copy; 2026 - Mi Proyecto Nuxt</footer>
+  </div>
+</template>
---
+++ nuxt-vue-exercises/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// Composition API
+const message = ref("¡Hola desde Nuxt 4!");
+
+// Esto le dice a Nuxt que use el archivo layouts/default.vue
+definePageMeta({
+  layout: "default",
+});
+</script>
+
+<template>
+  <section class="py-10">
+    <div class="bg-white p-8 rounded-lg shadow-md border-t-4 border-green-500">
+      <h2 class="text-2xl font-bold mb-4">{{ message }}</h2>
+      <p class="text-gray-600">
+        Si ves este recuadro con sombra y el borde verde, **Tailwind** está funcionando. Si ves el Header y Footer, el
+        **Layout** está funcionando.
+      </p>
+    </div>
+  </section>
+</template>
