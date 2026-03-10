cc58b6d9f9138123c8cb68154985b998676a2812
marcoslgl <marcoslgl2004@gmail.com>
2026-03-10 10:42:27 +0100

    feat(vue-nuxt) add exercise 5

--- nuxt-vue-exercises/components/TheCounter.vue
+++ nuxt-vue-exercises/components/TheCounter.vue
@@ -5,12 +5,19 @@ const count = ref(0);
 
 <template>
   <TheTitle>Count: {{ count }}</TheTitle>
-  <p class="text-gray-600 mb-4">This is a simple counter component.</p>
   <div class="flex items-center space-x-4">
-    <button @click="count--" class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 transition">
+    <button
+      v-if="count > 0"
+      @click="count--"
+      class="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition"
+    >
       Decrease
     </button>
-    <button @click="count++" class="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700 transition">
+    <button
+      v-if="count < 10"
+      @click="count++"
+      class="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition"
+    >
       Increase
     </button>
   </div>
